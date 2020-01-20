7ef4094731054fba5e4b0fd4337a4349f4f97060
tiagonapoli <tiago.napoli@vtex.com.br>
2020-01-20 18:22:39 -0300

    Migrate 'vtex setup'

---
+++ src/commands/setup.ts
@@ -0,0 +1,32 @@
+import { flags } from '@oclif/command'
+
+import { getManifest } from '../manifest'
+import { CustomCommand } from '../lib/CustomCommand'
+import { setupESLint } from '../modules/setup/setupESLint'
+import { setupTSConfig } from '../modules/setup/setupTSConfig'
+import { setupTypings } from '../modules/setup/setupTypings'
+
+const buildersToAddAdditionalPackages = ['react', 'node']
+const buildersToAddTypes = ['react', 'node']
+
+export default class Setup extends CustomCommand {
+  static description = 'Download react app typings, graphql app typings, lint config and tsconfig'
+
+  static examples = []
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    'ignore-linked': flags.boolean({ char: 'i', description: 'Add only types from apps published', default: false }),
+  }
+
+  static args = []
+
+  async run() {
+    const { flags } = this.parse(Setup)
+    const ignoreLinked = flags['ignore-linked']
+    const manifest = await getManifest()
+    setupESLint(manifest, buildersToAddAdditionalPackages)
+    await setupTSConfig(manifest)
+    await setupTypings(manifest, ignoreLinked, buildersToAddTypes)
+  }
+}

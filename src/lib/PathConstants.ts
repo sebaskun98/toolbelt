b0251ca595c28d106ed8df2ce043af2ba9908f73
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-04-01 00:39:50 -0300

    [PathConstants] Create cache related constants
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- src/lib/PathConstants.ts
+++ src/lib/PathConstants.ts
@@ -6,4 +6,7 @@ export class PathConstants {
 
   public static readonly PRETASKS_FOLDER = join(PathConstants.VTEX_FOLDER, 'pretasks')
   public static readonly TELEMETRY_FOLDER = join(PathConstants.VTEX_FOLDER, 'telemetry')
+  
+  public static readonly CACHE_FOLDER = join(PathConstants.VTEX_FOLDER, 'cache')
+  public static readonly MESSAGES_CACHE_FOLDER = join(PathConstants.CACHE_FOLDER, 'messages')
 }
